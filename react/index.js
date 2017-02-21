9caa6be7d0b8f901d4feb684314be387fcd62f6b
Paul Betts <paul@paulbetts.org>
2017-02-20 20:33:00 -0800

    Update React JS for HMR

--- react/index.js
+++ react/index.js
@@ -6,7 +6,9 @@ module.exports = {
     'react@^15.4.1',
     'react-dom@^15.4.1'
   ],
-  devDependencies: [],
+  devDependencies: [
+    'react-hot-loader@^3.0.0-beta.6',
+  ],
   templateDirectory: path.resolve(__dirname, './tmpl'),
   postCopy: (initDir, ora, lintStyle) => {
     // Do nothing
