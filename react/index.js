d2cce3150c55d8b0e63d212fdb61cc5fb7ba365d
Paul Betts <paul@paulbetts.org>
2017-02-20 20:53:33 -0800

    These have to be prod deps

--- react/index.js
+++ react/index.js
@@ -4,11 +4,10 @@ module.exports = {
   dependencies: [
     'electron-devtools-installer@^2.0.1',
     'react@^15.4.1',
-    'react-dom@^15.4.1'
-  ],
-  devDependencies: [
+    'react-dom@^15.4.1',
     'react-hot-loader@^3.0.0-beta.6',
   ],
+  devDependencies: [],
   templateDirectory: path.resolve(__dirname, './tmpl'),
   postCopy: (initDir, ora, lintStyle) => {
     // Do nothing
