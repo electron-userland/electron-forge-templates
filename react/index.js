d5bd6da5dce470c19d4ea108d40dcba0ee625cfe
Samuel Attard <samuel.r.attard@gmail.com>
2017-02-28 11:18:29 +1100

    Add required es2015 classes transform

--- react/index.js
+++ react/index.js
@@ -7,7 +7,9 @@ module.exports = {
     'react-dom@^15.4.1',
     'react-hot-loader@^3.0.0-beta.6',
   ],
-  devDependencies: [],
+  devDependencies: [
+    'babel-plugin-transform-es2015-classes'
+  ],
   templateDirectory: path.resolve(__dirname, './tmpl'),
   postCopy: (initDir, ora, lintStyle) => {
     // Do nothing
