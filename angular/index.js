17a3f253b86c0bb8bcad9a3a73f10ce6a6e2b105
Mark Lee <electronjs@lazymalevolence.com>
2018-04-04 16:40:53 -0400

    Update to Angular 5

--- angular/index.js
+++ angular/index.js
@@ -3,18 +3,18 @@ const path = require('path');
 
 module.exports = {
   dependencies: [
-    '@angular/common@^4.3.4',
-    '@angular/compiler@^4.3.4',
-    '@angular/core@^4.3.4',
-    '@angular/platform-browser@^4.3.4',
-    '@angular/platform-browser-dynamic@^4.3.4',
+    '@angular/common@^5.2.9',
+    '@angular/compiler@^5.2.9',
+    '@angular/core@^5.2.9',
+    '@angular/platform-browser@^5.2.9',
+    '@angular/platform-browser-dynamic@^5.2.9',
     'electron-devtools-installer@^2.2.0',
     'reflect-metadata@^0.1.10',
     'tslib@^1.7.1',
     'zone.js@^0.8.16'
   ],
   devDependencies: [
-    'tslint@^4.5.1',
+    'tslint@^5.9.1',
     'typescript@^2.4.2'
   ],
 
